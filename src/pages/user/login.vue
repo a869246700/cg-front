c3bdd0c5f2d31e172ddbc24d7e9ae8e899abba35
Goo <gaoyu@ffcs.cn>
2022-02-22 00:49:42 +0800

    lf

--- src/pages/user/login.vue
+++ src/pages/user/login.vue
@@ -2,10 +2,14 @@
   <div class="test">用户登录: {{ userStore.name }}, 长度:{{ userStore.nameLength }}, 角色: {{ userStore.role }}</div>
   <button @click="updateName">修改store中的name</button>
   <button @click="updateNameByAction">修改store中的name</button>
+
+  <div>{{ obj.name + ' -- ' + obj.star }}</div>
+  <button @click="starAdd">star</button>
 </template>
 
 <script lang="ts" setup name="UserLogin">
 import { useUserStore } from '@/store/user'
+import { reactive, watch } from 'vue'
 
 const userStore = useUserStore()
 
@@ -18,6 +22,17 @@ const updateName = () => {
 const updateNameByAction = () => {
   userStore.getUsername()
 }
+
+const obj = reactive({
+  name: 'Goo',
+  age: 18,
+  star: 0
+})
+const starAdd = () => (obj.star += 1)
+
+watch(obj, (nv, ov) => {
+  console.log(nv, ov)
+})
 </script>
 
 <style lang="scss" scoped>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
   {
+    path: '/',
+    redirect: '/user_login'
+  },
+  {
     path: '/user_login',
     name: 'UserLogin',
     component: () => import('@/pages/user/login.vue') // 注意这里要带上 文件后缀.vue
